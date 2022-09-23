e8fd4e07dd560dc4441ac92ce9ab76fc4b078d7e
Cédric Gauthier <cedric.gauthier@captivea.com>
2022-09-23 18:09:28 +0200

    [ADD] question

--- swapi_quizz/models/quizz.py
+++ swapi_quizz/models/quizz.py
@@ -20,12 +20,13 @@ class Quizz(models.Model):
     level_id = fields.Many2one(comodel_name='swapi.level',
                                        string='Level',
                                        required=True)
-    question = fields.Text(string="Question")
+    states = fields.Text(string="Question")
+    proposition = fields.Char(string="Proposition")
     
     @api.onchange('player_id','theme_id','level_id')
     def _onchange_name(self):
         self.name = str(self.player_id.name).upper() + " / " + str(self.theme_id.name) + " * " + str(self.level_id.name)
-    
+        self.states = "Dans quel(s) film(s) peut-on voir cette espèce ?"
     def start_quizz(self):
         
         if self.level_id.name == "Padawan" :
@@ -33,7 +34,7 @@ class Quizz(models.Model):
                 specie_count = self.env['swapi.specie'].search_count([])
                 random_number = random.randint(1,specie_count)
                 specie_random = self.env['swapi.specie'].search([('id',"=",random_number)])
-                self.question = "Dans quel(s) film(s) peut-on voir cette espèce ?" + str(specie_random.name)
+                self.proposition = str(specie_random.name)
                 # raise UserError(str(specie_random.name))
                 
                      
