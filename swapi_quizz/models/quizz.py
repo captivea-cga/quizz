f32b161678a3fa3e9491c17a70e4a29302b8707a
Cédric Gauthier <cedric.gauthier@captivea.com>
2022-09-23 13:30:08 +0200

    quizz model and view

---
+++ swapi_quizz/models/quizz.py
@@ -0,0 +1,26 @@
+#  -*- coding: utf-8 -*-
+
+
+from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
+
+
+class Quizz(models.Model):
+    
+    _name = 'swapi.quizz'
+    _description = 'Quizz Info'
+    
+    name = fields.Char(string="Name")
+    player_id = fields.models.Many2One(comodel_name='swapi.player',
+                                       string='Player',
+                                       required=True)
+    theme_id = fields.models.Many2One(comodel_name='swapi.theme',
+                                       string='Theme',
+                                       required=True)
+    level_id = fields.models.Many2One(comodel_name='swapi.level',
+                                       string='Level',
+                                       required=True)
+    
+    
+    
+    
