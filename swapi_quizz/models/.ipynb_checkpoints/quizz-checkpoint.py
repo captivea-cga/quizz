20b2054b32e2dc4cd53d9564e70cc9fbe7358e30
Cédric GAUTHIER <cedric.gauthier@captivea.com>
2022-10-06 16:54:55 +0000

    [ADD] questions and proposals

--- swapi_quizz/models/.ipynb_checkpoints/quizz-checkpoint.py
+++ swapi_quizz/models/.ipynb_checkpoints/quizz-checkpoint.py
@@ -11,37 +11,52 @@ class Quizz(models.Model):
     _description = 'Quizz Info'
     
     name = fields.Char(string="Name")
-    # player_id = fields.Many2one(comodel_name='swapi.player',
-    #                                    string='Player',
-    #                             )
+    
     theme_id = fields.Many2one(comodel_name='swapi.theme',
                                        string='Theme',
-                                       required=True)
+                                       required=True, readonly=True)
     level_id = fields.Many2one(comodel_name='swapi.level',
                                        string='Level',
-                                       required=True)
+                                       required=True, readonly=True)
     
-#     states = fields.Text(string="Question")
-#     proposition = fields.Char(string="Proposition")
-#     start_quizz = fields.Boolean(string="Start_quizz", default=False)
-    # @api.onchange('player_id','theme_id','level_id')
-    # def _onchange_name(self):
-    #     self.name = str(self.player_id.name).upper() + " / " + str(self.theme_id.name) + " * " + str(self.level_id.name)
-    #     if self.level_id.name == "Padawan" :
-    #         if self.theme_id.name =="Species":
-    #             self.states = "Dans quel(s) film(s) peut-on voir cette espèce ?"
-    #         else:
-    #             self.states = "Il n'y a pas encore de question pour ce thème/niveau"
-    def start_quizz_species_padawan(self):
-#         self.start_quizz = True
-        specie_count = self.env['swapi.specie'].search_count([])
-        random_number = random.randint(1,specie_count)
+    player_id = fields.Many2one(comodel_name='swapi.player',
+                                string='Player')
+    score = fields.Float("Score",readonly=True)
+    question_nr = fields.Integer ("Number of question",readonly=True)
+    question = fields.Char(string="Question")
+    response_specie_ids = fields.Many2many(comodel_name='swapi.specie',
+                                   string='Response(s)')
+    is_quizz_started = fields.Boolean(string="Start_quizz",default=False)
+    question_spinner = fields.Integer(string="Question nr")
+    question_template = fields.Char()            
+    proposal_nr = fields.Integer(string="Proposal nr")
+    
+    def start_quizz(self):
+        self.is_quizz_started = True
+        question_nr = self.env['swapi.specie'].search_count([])
+        self.question_nr = question_nr
+        random_number = random.randint(1,question_nr)
         specie_random = self.env['swapi.specie'].search([('id',"=",random_number)])
-#         self.states = self.env['swapi.question_template'].search(["&",('theme_id.id','=',self.theme_id.id),('level_id.id','=',self.level_id.id)]).question_template
-#         self.proposition = str(specie_random.name)
-        # raise UserError(str(specie_random.name))
-                
-    # Faire une fonction pour créer les quizz (tous) et faire un boutton dans la vue liste : Create All Quizz
-    def create_all_quizz(self):
-        raise UserError("All Quizz")           
+        
+        self.question_spinner = 1
+        question_template = self.env['swapi.question_template'].search(["&",('theme_id.id','=',self.theme_id.id),('level_id.id','=',self.level_id.id)])
+        self.question_template = question_template.question_template
+        self.question = specie_random.name
+    
+    def next_question(self):
+        if self.question_spinner == self.question_nr :
+            self.is_quizz_started = False
+        random_number = random.randint(1,self.question_nr)
+        specie_random = self.env['swapi.specie'].search([('id',"=",random_number)])
+        self.question = specie_random.name
+        self.question_spinner +=1
+        if self.level_id.id == 1 :
+           self.proposal_nr = specie_random.film_ids.count 
+        
+#         if self.level_id == "Padawan" :
+#              if self.theme_id =="Species":
+#                 random_number = random.randint()
+#                 specie_random = self.env['swapi.specie'].search([('id',"=",random_number)])
+#                 if specie_random:
+#                     self.question = specie_random.name            
                      
--- swapi_quizz/wizard/.ipynb_checkpoints/quizz_wizard-checkpoint.py
+++ swapi_quizz/wizard/.ipynb_checkpoints/quizz_wizard-checkpoint.py
@@ -28,18 +28,44 @@ class QuizzWizard(models.TransientModel):
     player_id = fields.Many2one(comodel_name='swapi.player',
                                 string='Player',
                                 required=True)
+    score = fields.Float("Score",readonly=True)
+    question_nr = fields.Integer ("Number of question",readonly=True)
     question = fields.Char(string="Question")
     response_specie_ids = fields.Many2many(comodel_name='swapi.specie',
                                    string='Response(s)')
     
-    def start_quizz(self):
-        if self.level_id == "Padawan" :
+    
+#     def start_quizz(self):
+#         if self.level_id == "Padawan" :
+#              if self.theme_id =="Species":
+#                 random_number = random.randint()
+#                 specie_random = self.env['swapi.specie'].search([('id',"=",random_number)])
+#                 if specie_random:
+#                     self.question = specie_random.name     
+    def next_question(self):
+         if self.level_id == "Padawan" :
              if self.theme_id =="Species":
                 random_number = random.randint()
                 specie_random = self.env['swapi.specie'].search([('id',"=",random_number)])
                 if specie_random:
-                    self.question = specie_random.name     
+                    self.question = specie_random.name  
         
+    @api.onchange('player_id','theme_id')
+    def _onchange_score(self):
+        score_player = self.env['swapi.player'].search([('id','=',self.player_id.id)]).score_total
+        self.score = score_player
         
-        
-        
+    @api.onchange('theme_id')
+    def _onchange_theme(self):
+        if self.theme_id.id == 1:
+            self.question_nr = self.env['swapi.specie'].search_count([])
+        if self.theme_id.id == 2:
+            self.question_nr = self.env['swapi.planet'].search_count([])
+        if self.theme_id.id == 3:
+            self.question_nr = self.env['swapi.film'].search_count([])
+        if self.theme_id.id == 4:
+            self.question_nr = self.env['swapi.people'].search_count([])
+        if self.theme_id.id == 5:
+            self.question_nr = self.env['swapi.starship'].search_count([])
+        if self.theme_id.id == 6:
+            self.question_nr = self.env['swapi.vehicle'].search_count([])
